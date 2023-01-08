60135db267832b9ea29b9ace87bbf89aee4af1fa
wattgit <muawatt@gmail.com>
2023-01-09 03:25:05 +0700

    swap app

--- server.js
+++ server.js
@@ -21,20 +21,19 @@ async function connect() {
 }
 connect();
 
-
-// Go to full url which is mapped with genShort (the link we've made) 
-app.get('/:message', async (req, res) => {
-    const genShort = req.params.message;
-    const result = await database.collection('urls').findOne({ genShort });
-    res.redirect(result.url);
-});
-
 app.post('/', async (req, res) => {
     const url = req.body;
     const genShort = randomstring.generate(6);
     const obj = { url, genShort};
     await database.collection('urls').insertOne(obj);
     res.send(obj);
+});
+
+// Go to full url which is mapped with genShort (the link we've made) 
+app.get('/:message', async (req, res) => {
+    const genShort = req.params.message;
+    const result = await database.collection('urls').findOne({ genShort });
+    res.redirect(result.url);
 });
 
 app.listen(8000, () => console.log('Server is running'));
